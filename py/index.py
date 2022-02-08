0b8ff1abf2c6e83da715161f19fb541a02b4c9a3
blitzher <skovbooorg@gmail.com>
2022-02-08 01:43:43 +0100

    base

--- py/index.py
+++
@@ -1,9 +0,0 @@
-from lexer import Lexer
-
-def main():
-	lexer = Lexer("../tests/eggs.rcp")
-
-	print(lexer.fields)
-	
-if __name__ == "__main__":
-	main()
--- ruby/index.rb
+++
@@ -1,9 +0,0 @@
-
-require_relative "lexer.rb"
-
-def main()
-	lexer = Lexer::Lexer.new "../tests/eggs.rcp"
-
-end
-
-main()
