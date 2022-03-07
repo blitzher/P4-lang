58157476f8d2f65e3545b79319c983e813182326
Jaffenheimer <jfog20@student.aau.dk>
2022-03-07 13:39:58 +0100

    Added output gen 1 draft, able to output recepi into template HTML.
    Co-authored-by: blitzher <blitzher@users.noreply.github.com>

--- src/outgen.cpp
+++ src/outgen.cpp
@@ -1,32 +1,99 @@
-
 #include "epicr.h"
+#include <filesystem>
 
-#define PRINT_VEC(vec) { for(auto v: vec) file << v << ", "; file << std::endl;}
+using std::string;
+namespace fs = std::filesystem;
 
-namespace epicr {
-	bool generate_html(recipe rcp, std::string filename) {
+#define MAX_S_LENGTH 4096
+#define MAX_B_Lenght 65536 /* 4096 * 16 */
+namespace epicr
+{
+
+	string load_template(string template_name)
+	{
+		string file_content;
+		string dir = "src";
+		string file_name = template_name + ".html";
+		fs::path fpath = dir;
+		fpath /= "output-templates";
+		fpath /= file_name;
+		std::ifstream fstream;
+		fstream.open(fpath, std::istream::in);
+
+		int gotten_char = fstream.get();
+		do
+		{
+			file_content += gotten_char;
+			gotten_char = fstream.get();
+		} while (!fstream.eof());
+
+		return file_content;
+	}
+
+	bool generate_html(recipe rcp, string filename)
+	{
+
+		string base_template_s = load_template("base");
+		const char *base_template = base_template_s.c_str();
+		string step_template_s = load_template("step");
+		const char *step_template = step_template_s.c_str();
 
 		std::ofstream file{filename};
 		if (!file.is_open())
 			return false;
 
-		file << rcp.title << std::endl;
-
-		file << rcp.time << std::endl;
-
-		file << rcp.description << std::endl;
-
-		PRINT_VEC(rcp.tags);
-		file << std::endl;
-
-		PRINT_VEC(rcp.kitchenware);
-		file << std::endl;
-
-		for (auto ingr : rcp.ingredients) {
-			file << ingr.name << std::endl;
-			file << ingr.amount << std::endl;
-			file << ingr.unit << std::endl;
+		for (auto instr : rcp.instructions)
+		{
+			std::cout << "instr found!" << std::endl;
 		}
+
+		string instructions_section = "";
+		char *instr_s = (char *)malloc(MAX_S_LENGTH);
+		char *base_s = (char *)malloc(MAX_B_Lenght);
+		int indx = 1;
+		for (auto inst : rcp.instructions)
+		{
+			string ingredients = "";
+			for (auto ingr : inst.ingredients)
+				ingredients += ingr.name + " ";
+
+			string body = "";
+			for (auto word : inst.body)
+				body += word.word + " ";
+
+			string step_text = "Step " + std::to_string(indx);
+			sprintf(instr_s, step_template, step_text.c_str(), ingredients.c_str(), body.c_str());
+			instructions_section += instr_s;
+		}
+
+		string tags = "";
+		for (auto tag : rcp.tags)
+		{
+			tags += tag + " ";
+		}
+
+		string ingredients = "";
+		for (auto ingredient : rcp.ingredients)
+		{
+			ingredients += ingredient.name + " ";
+		}
+
+		string kitchenware = "";
+		for (auto ware : rcp.kitchenware)
+		{
+			kitchenware += ware + " ";
+		}
+
+		sprintf(base_s, base_template,
+				rcp.title.c_str(),
+				rcp.description.c_str(),
+				rcp.time.c_str(),
+				tags.c_str(),
+				ingredients.c_str(),
+				kitchenware.c_str(),
+				instructions_section.c_str());
+		file << base_s << std::endl;
+		file.close();
 
 		return true;
 	}
