6e917ab199d87f033a11b5fcebecd2c910a7e305
Jaffenheimer <jfog20@student.aau.dk>
2022-04-04 09:32:11 +0200

    Alignment of buttons and label in gui

--- src/GUI.py
+++ src/GUI.py
@@ -1,34 +1,33 @@
-""" import os
+import os
 import tkinter as tk
 from tkinter import ttk
 import tkinter.filedialog as fd
+from turtle import color
 
 
 class App(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
-
+        root.geometry("500x75")
+        self.grid_configure(column=10, row=10)
         self.input_file_button = tk.Button(
-            self, command=lambda: self.open_file(self.input_fpath), text="Open file")
+            self, command=lambda: self.open_file(self.input_fpath), text="Open file", height=1, width=10)
         self.output_dir_button = tk.Button(
-            self, command=lambda: self.open_dir(self.out_dir), text="Open output dir")
-
-        self.input_file_button.grid(column=0, row=0, sticky=tk.W, pady=2)
-        self.output_dir_button.grid(column=0, row=1, sticky=tk.W, pady=2)
-
+            self, command=lambda: self.open_dir(self.out_dir), text="Open output dir", height=1, width=10)
+        self.input_file_button.pack()
+        self.output_dir_button.pack()
         self.input_fpath = tk.StringVar()
         self.input_fpath.set("No file selected")
         self.input_fpath_label = tk.Label()
         self.input_fpath_label["textvariable"] = self.input_fpath
-
         self.output_fpath_label = tk.Label()
         self.out_dir = tk.StringVar()
         self.out_dir.set("No directory selected")
         self.output_fpath_label["textvariable"] = self.out_dir
 
-        self.input_fpath_label.grid(column=1, row=0, padx=30, pady=2)
-        self.output_fpath_label.grid(column=1, row=1, padx=30, pady=2)
+        self.input_fpath_label.place(x=130, y=3)
+        self.output_fpath_label.place(x=130, y=33)
 
     def open_file(self, dest):
         file = fd.askopenfile(filetypes=[('epicR Files', '*.rcp')])
@@ -54,8 +53,7 @@ class App(tk.Frame):
 root = tk.Tk()
 app = App(root)
 app.mainloop()
- """
-
+"""
 # import tkinter module
 from tkinter import *
 from tkinter.ttk import *
@@ -83,3 +81,4 @@ e2.grid(row=1, column=1, pady=2)
 # infinite loop which can be terminated by keyboard
 # or mouse interrupt
 mainloop()
+"""
