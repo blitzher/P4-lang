73cf681d9a5c5b3ff89e3ad0dcfa55aff1f31ab7
blitzher <skovbooorg@gmail.com>
2022-05-10 12:38:44 +0200

    added namespace to functioncall

--- src/main.cpp
+++ src/main.cpp
@@ -39,24 +39,25 @@ int main(int argc, char **argv)
 		{
 			string output_file = epicr::clargs.output_filepath;
 			string filename = (std::filesystem::path(epicr::clargs.input_filepath).stem()).string();
-			
+
 			epicr::epicr_html_style choosen_style = epicr::clargs.choosen_style;
-			//epicr::epicr_html_style choosen_style = epicr::E_OS_FANCY;  // switch with above for html output also in parser
-			if(choosen_style == epicr::E_OS_FANCY) {
+			// epicr::epicr_html_style choosen_style = epicr::E_OS_FANCY;  // switch with above for html output also in parser
+			if (choosen_style == epicr::E_OS_FANCY)
+			{
 				std::filesystem::path file = output_file / std::filesystem::path(filename);
 				output_file = file.string() + ".html";
 				epicr::generate_html(my_recipe, output_file);
 				printf("Wrote HTML file: %s\n", output_file.c_str());
 			}
-			else if (choosen_style == epicr::E_OS_BASIC){
+			else if (choosen_style == epicr::E_OS_BASIC)
+			{
 				std::filesystem::path file = output_file / std::filesystem::path(filename);
 				output_file = file.string() + ".html";
-				generate_basic(my_recipe, output_file);
+				epicr::generate_basic(my_recipe, output_file);
 				printf("Wrote basic file: %s\n", output_file.c_str());
 			}
 		}
 	}
-
 
 	return 0;
 }
