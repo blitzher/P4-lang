fb79f9b8d2b6344536c5a4b305818aba0f78df4d
Jaffenheimer <jfog20@student.aau.dk>
2022-05-10 14:17:12 +0200

    Added default selected buttons in gui

--- src/GUI.py
+++ src/GUI.py
@@ -83,7 +83,7 @@ class App(tk.Frame):
         self.out_dir_label = tk.StringVar()
         self.out_dir_label.set("No directory selected")
         self.output_fpath_label["textvariable"] = self.out_dir_label
-        self.html_style = ""
+        self.html_style = "Basic"
         self.unit_system = "None"
         self.directory = tk.StringVar()
         self.directory.set("")
@@ -91,6 +91,8 @@ class App(tk.Frame):
         self.output_dir_clarg.set("-o ")
         self.input_fpath_label.place(x=175, y=3)
         self.output_fpath_label.place(x=175, y=33)
+        self.html_buttons.set(2)
+        self.unit_buttons.set(3)
 
     def open_file(self, dest):
         file = fd.askopenfile(filetypes=[("epicR Files", "*.rcp")])
@@ -139,7 +141,6 @@ class App(tk.Frame):
             self.html_style,
             self.unit_system,
         )
-        print(self.unit_system)
         os.system("%s %s" % (executable, clargs))
 
     def format_fpath(self, s):
